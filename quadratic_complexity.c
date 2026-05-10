6b383fa3ae6b056a6755fe050e680cb6938f76fd
Sekqies <fernando.ellerbrock@gmail.com>
2026-05-09 22:57:09 -0300

    quadratic complexity algorithms

---
+++ quadratic_complexity.c
@@ -0,0 +1,41 @@
+void swap(int* a,int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int min(int a, int b){
+    return a>b? a : b;
+}
+
+void bubble_sort(int* ar, int n){
+    for(int i = 0; i < n; ++i){
+        for(int j = 1; j < n; ++j){
+            if(ar[j] < ar[j-1]){
+                swap(&ar[j], &ar[j-1]);
+            } 
+        }
+    }
+}
+
+void selection_sort(int* ar, int n){
+    for(int i = 0; i < n; ++i){
+        int smallest_index = i;
+        for(int j = 0; j < n; ++j){
+            if(ar[i] > ar[j]) smallest_index = j;
+        }
+        swap(ar[i],ar[j]);
+    }
+}
+
+void insert_sort(int* ar, int n){
+    for(int i = 1; i < n; ++i){
+        int j = i-1;
+        int key = ar[i];
+        while(j >= 0 && ar[j] > key){
+            ar[j+1] = ar[j];
+            j--;
+        }
+        ar[j+1] = key;
+    }
+}
